2215ca747d050ea8699cb43e37fcc43d04b74ec4
mkamra2000 <kamra29mayank@gmail.com>
2022-01-05 10:51:11 +0530

    Portfolio Website Initialised

---
+++ index.js
@@ -0,0 +1,8 @@
+function hello(){
+    if(document.getElementById("uls").style.display==="none"){
+        document.getElementById("uls").style.display="block";
+    }
+    else{
+        document.getElementById("uls").style.display="none";
+    }
+}
