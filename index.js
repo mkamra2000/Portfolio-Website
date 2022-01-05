7a5aceb9ef3669e2957b684d442336d88a57f855
mkamra2000 <kamra29mayank@gmail.com>
2022-01-05 11:16:53 +0530

    Hamburger Menu Improved

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-function hello(){
-    if(document.getElementById("uls").style.display==="none"){
-        document.getElementById("uls").style.display="block";
-    }
-    else{
-        document.getElementById("uls").style.display="none";
-    }
+function hello() {
+  if (document.getElementById("uls").style.display === "none") {
+    document.getElementById("uls").style.display = "block";
+    window.addEventListener("click", function (e) {
+      if (document.getElementById("uls").contains(e.target)) {
+        document.getElementById("uls").style.display = "none";
+      }
+    });
+  } else {
+    document.getElementById("uls").style.display = "none";
+  }
 }
