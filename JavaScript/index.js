c6a4191a3983a3ee11d82e2c0216e37bed866435
Mayank Kamra <kamra29mayank@gmail.com>
2022-05-10 21:37:23 +0530

    Version 2 Completed

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,17 @@
 function hgClick() {
   if (document.getElementById("uls").style.display === "none") {
     document.getElementById("uls").style.display = "block";
-    window.addEventListener("click", function (e) {
+    document.addEventListener("click", function (e) {
       if (document.getElementById("uls").contains(e.target)) {
+        document.getElementById("uls").style.display = "none";
+      }
+      if(document.getElementById("contactForm") && document.getElementById("contactForm").contains(e.target)){
+        document.getElementById("uls").style.display = "none";
+      }
+      if(document.getElementById("thanks") && document.getElementById("thanks").contains(e.target)){
+        document.getElementById("uls").style.display = "none";
+      }
+      if(document.getElementById("mainContainer") && document.getElementById("mainContainer").contains(e.target)){
         document.getElementById("uls").style.display = "none";
       }
     });
