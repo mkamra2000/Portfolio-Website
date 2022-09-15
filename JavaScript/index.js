514de9ff5e933bd062f9075b78c3993b46001c9b
Mayank Kamra <kamra29mayank@gmail.com>
2022-09-15 23:41:51 +0530

    Scroll Animation Added

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,4 @@
+let interval = setInterval(checkViewport,400);
 function hgClick() {
   if (document.getElementById("uls").style.display === "none") {
     document.getElementById("uls").style.display = "block";
@@ -5,13 +6,22 @@ function hgClick() {
       if (document.getElementById("uls").contains(e.target)) {
         document.getElementById("uls").style.display = "none";
       }
-      if(document.getElementById("contactForm") && document.getElementById("contactForm").contains(e.target)){
+      if (
+        document.getElementById("contactForm") &&
+        document.getElementById("contactForm").contains(e.target)
+      ) {
         document.getElementById("uls").style.display = "none";
       }
-      if(document.getElementById("thanks") && document.getElementById("thanks").contains(e.target)){
+      if (
+        document.getElementById("thanks") &&
+        document.getElementById("thanks").contains(e.target)
+      ) {
         document.getElementById("uls").style.display = "none";
       }
-      if(document.getElementById("mainContainer") && document.getElementById("mainContainer").contains(e.target)){
+      if (
+        document.getElementById("mainContainer") &&
+        document.getElementById("mainContainer").contains(e.target)
+      ) {
         document.getElementById("uls").style.display = "none";
       }
     });
@@ -20,7 +30,25 @@ function hgClick() {
   }
 }
 
-function load(){
+function load() {
   let loader = document.getElementById("loading");
-  loader.style.display = 'none';
+  loader.style.display = "none";
+}
+
+function checkViewport() {
+  var myElement = document.getElementById("skills");
+  var progress1 = document.getElementById("progress1");
+  var progress2 = document.getElementById("progress2");
+  var bounding = myElement.getBoundingClientRect();
+
+  if (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.right <= window.innerWidth &&
+    bounding.bottom <= window.innerHeight
+  ) {
+    progress1.style.animation = 'progress1 1.5s ease-in-out';
+    progress2.style.animation = 'progress2 1.5s ease-in-out';
+    clearInterval(interval);
+  }
 }
